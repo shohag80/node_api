b253dfda743a5fc98cbf31171323ec9af2258a80
Md. Shohag Hossain <shohag@atilimited.net>
2024-09-25 09:01:15 +0600

    remove deprecated

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ const app = express();
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected');
-}).catch((err) => {
-    console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
 // Routes
 app.use(userRoutes);
